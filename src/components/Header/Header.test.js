069e8757f40ca48393af3f92289f28c30da3fc2f
Marius <marius@example.com>
2022-06-27 09:36:21 +0300

    2 headings

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -6,8 +6,31 @@ it('renders Header', () => {
 });
 
 it('renders title from props', () => {
+  // Arrange
   render(<Header title='testing123' />);
   // const headerEl = screen.getByText(/testing123/i);
+  // Act
   const headerEl = screen.getByText('testing123');
+  // Assert
   expect(headerEl).toBeInTheDocument();
+});
+
+// it('should rendder a heading', () => {
+//   // Arrange
+//   render(<Header title='todos' />);
+
+//   // seacrh
+//   const headingEl = screen.getByRole('heading');
+//   // Assert
+//   expect(headingEl).toBeInTheDocument();
+// });
+
+it('should render 2 headings', () => {
+  // Arrange
+  render(<Header title='todos' />);
+
+  // seacrh
+  const headingElArr = screen.getAllByRole('heading');
+  // Assert
+  expect(headingElArr).toHaveLength(2);
 });
