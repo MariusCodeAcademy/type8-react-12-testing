99b6c8758eed27b98d42bec171afe3b169013b9b
Marius <marius@example.com>
2022-06-28 09:58:37 +0300

    mock server with msw

--- src/components/FollowersList/FollowersList.js
+++ src/components/FollowersList/FollowersList.js
@@ -12,6 +12,7 @@ export default function FollowersList() {
 
   const fetchFollowers = async () => {
     const { data } = await axios.get('https://randomuser.me/api/?results=5');
+    // console.log('data ===', data);
     setFollowers(data.results);
   };
 
--- src/components/FollowersList/FollowersList.test.js
+++ src/components/FollowersList/FollowersList.test.js
@@ -1,6 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import FollowersList from './FollowersList';
 import { BrowserRouter } from 'react-router-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { fullMockResponse } from './fullResponse';
+
+const mockResponse = {
+  results: [
+    {
+      cell: 'lakjdlaskjdkalsjdlkas',
+      name: {
+        first: 'James',
+        last: 'Bond',
+      },
+      picture: { large: 'https://randomuser.me/api/portraits/men/97.jpg' },
+      login: { username: 'randomuser' },
+    },
+  ],
+};
+
+// mock servert for simulating response
+const server = setupServer(
+  rest.get('https://randomuser.me/api/?results=5', (req, res, ctx) => {
+    return res(ctx.json(fullMockResponse));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 const FollowersListWithRouter = (args) => (
   <BrowserRouter>
